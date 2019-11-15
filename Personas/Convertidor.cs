993bfe9cdaaecb05d1ad1826d270a5b5f07abaa9
josellorca <jose.llorca@iesdoctorbalmis.com>
2019-11-15 10:16:34 +0100

    Agregar archivos de proyecto.

---
+++ Personas/Convertidor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Personas
+{
+    class Convertidor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().ToLower() == "hombre")
+            {
+                return "hombre.png";
+            }
+            else if(value.ToString().ToLower()=="mujer")
+            {
+                return "mujer.png";
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Personas/Persona.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace Personas
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public string Sexo { get; set; }
+
+        public Persona(int id, string nombre, string foto, string sexo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Foto = foto;
+            Sexo = sexo;
+        }
+
+        public static ObservableCollection<Persona> GetPersonas()
+        {
+            ObservableCollection<Persona> lista = new ObservableCollection<Persona>();
+
+            lista.Add(new Persona(1, "Pepe", "1.jpg", "Hombre"));
+            lista.Add(new Persona(2, "Antonio", "2.jpg", "Hombre"));
+            lista.Add(new Persona(3, "Sara", "3.jpg", "Mujer"));
+
+            return lista;
+        }
+    }
+
+}
